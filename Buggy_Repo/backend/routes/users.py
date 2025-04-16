51db8e9ffe2f52d074d650ca1c071f5a9e04a804
meet2701 <parekhmeet997@gmail.com>
2025-04-16 16:34:48 +0530

    Change in users.py and answer to easter egg

--- Buggy_Repo/backend/routes/users.py
+++ Buggy_Repo/backend/routes/users.py
@@ -8,7 +8,7 @@ async def get_users_collection():
     from db import init_db
     return init_db()["users_collection"]
 
-@router.get("/") # post to get changed by divy
+@router.get("/users") # post to get changed by divy # Changed / to /users
 async def get_users():
     collection = await get_users_collection()
     users = []
@@ -18,6 +18,7 @@ async def get_users():
     return users
 
 # whats ur favorite genre of music ??? mine is EDM
+# Mine is Pop
 @router.post("/")
 async def create_user(user: User):
     collection = await get_users_collection()
