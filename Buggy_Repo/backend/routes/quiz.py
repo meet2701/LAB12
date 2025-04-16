46457883406471519f52fcd3bf2765ec033284c1
Divy7106 <divyamp7106@gmail.com>
2025-04-16 15:29:02 +0530

    update in quiz.py

--- Buggy_Repo/backend/routes/quiz.py
+++ Buggy_Repo/backend/routes/quiz.py
@@ -41,14 +41,14 @@ game_state = {"high_score": 0}
 # god would hate me for not dockerizing this repo
 @router.get("/question")
 async def get_question():
-    question = questions[1]
+    question = random.choice(questions) # by divya, before : question = question[1], after : question = random.choice(question)
     return {
         "id": question["id"],
         "text": question["text"],
         "options": question["options"]
     }
 
-@router.get("/answer")
+@router.post("/answer") # get to post chenged by divya
 async def submit_answer(data: dict):
     question_id = data.get("id")
     answer = data.get("answer")
